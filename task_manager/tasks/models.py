68101bd667b0e645bf08fd7e6af89368098fa21d
Joao <adm@MacBook-Air-de-ADM.local>
2024-08-31 22:58:17 -0300

    :white_check_mark: Test CRUD operations for tasks

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    time = models.TimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
